d7e29fe8b23eccbba4a8fb93f47966925c3c2c8b
Hugo Luiz <hugoodesa@gmail.com>
2025-09-24 21:50:05 -0300

    feat: project started

---
+++ src/main/java/br/com/stapassoli/crud/CrudSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.stapassoli.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stapassoli/crud/CrudSpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stapassoli.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
